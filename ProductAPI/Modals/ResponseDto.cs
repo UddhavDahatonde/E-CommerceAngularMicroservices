d259436ff2047a849677c5671f46b90e010eb8bc
UddhavDahatonde <112875520+UddhavDahatonde@users.noreply.github.com>
2024-03-20 14:20:04 +0530

    ProductApi delete

--- ProductAPI/Modals/ResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductAPI.Modals
-{
-	public class ResponseDto
-	{
-        public object? Result { get; set; }
-        public string? Message { get; set; }
-        public bool? IsSuccess { get; set; }
-    }
-}
